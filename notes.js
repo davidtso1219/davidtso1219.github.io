ce0108a4c96830c10da215485596e5d7d581629e
david tso <davidtso1219@gmail.com>
2022-01-03 12:00:26 +0800

    Finished the first version of the designs of markdown

--- notes.js
+++ notes.js
@@ -6,11 +6,29 @@ const md_path = "./notes/" + quarter + "/" + course + ".md"
 const content = document.getElementsByClassName("content")[0]
 document.getElementsByTagName("title")[0].innerHTML = course.replace(/_/, ' ').toUpperCase()
 
+var md = new Remarkable({
+    // Modify the generated HTML by highlighting the code directly
+    highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+            try {
+                return hljs.highlight(lang, str).value;
+            } catch (err) {}
+        }
+
+        try {
+            return hljs.highlightAuto(str).value;
+        } catch (err) {}
+
+        return ''; // use external default escaping
+    }
+});
+
+// rest of your code
+
 async function fill_content(file_path) {
     const response = await fetch(file_path)
     const txt = await response.text()
-    const conv = new showdown.Converter()
-    content.innerHTML = conv.makeHtml(txt)
+    content.innerHTML = md.render(txt)
 }
 
 fill_content(md_path)
