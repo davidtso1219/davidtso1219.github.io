b56c601132448314e35ddd985c6e180b1868cf7c
david tso <davidtso1219@gmail.com>
2022-01-04 20:53:04 +0800

    Added testnote

--- notes.js
+++ notes.js
@@ -4,6 +4,7 @@ const quarter = urlParams.get('quarter')
 const course = urlParams.get("class")
 const md_path = "./notes/" + quarter + "/" + course + ".md"
 const content = document.getElementsByClassName("content")[0]
+const body = document.getElementsByTagName('body')[0]
 document.getElementsByTagName("title")[0].innerHTML = course.replace(/_/, ' ').toUpperCase()
 
 var md = new Remarkable({
@@ -31,4 +32,36 @@ async function fill_content(file_path) {
     content.innerHTML = md.render(txt)
 }
 
-fill_content(md_path)
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+
+
+async function main() {
+    await fill_content(md_path)
+
+    const ps = document.getElementsByTagName('p')
+    for (var i = 0; i < ps.length; i++) {
+        var p = ps[i]
+        if (typeof p.previousElementSibling !== 'undefined' && p.previousElementSibling && (p.previousElementSibling.tagName.toLowerCase() === 'p' || p.previousElementSibling.tagName.toLowerCase() == 'ol' || p.previousElementSibling.tagName.toLowerCase() == 'ul')){
+            p.style.margin = 0;
+        }
+    }
+
+    window.onload = function() {
+        var header = document.getElementsByTagName('h1')[0]
+        header.setAttribute('data-text', '-' + header.textContent)
+
+        header.onclick = function() {
+            content.classList.add('shake')
+            setTimeout(() => { content.classList.remove('shake') }, 500)
+        }
+    }
+}
+
+main()
+
---
+++ notes/winter_2022/test_note.md
@@ -0,0 +1,51 @@
+#  Test Note
+
+## Intro
+
+My favorite language is `python`
+
+```py
+import numpy as np
+
+for i in range(10):
+    print(i)
+```
+
+***
+
+I also know a few languages:
+- Javascript
+- C++
+- Java
+- HTML/CSS
+
+***
+
+Here are my hobbies:
+1. Play Basketball
+2. Practice Leetcode
+
+***
+
+Here is my favorite quote:
+
+> It’s the one thing you can control. You are responsible for how people remember you—or don’t. So don’t take it lightly.
+>
+> -- Kobe Bryant
+
+***
+
+Here are the grades I got for Fall 2021 @ UCSD
+
+| Classes | Grades |
+| -- | -- |
+| CSE 21 | A |
+| CSE 15L | A |
+| EDS 124BR | A+ |
+| ECON 1A | A |
+| WCWP 100 | A- |
+
+***
+
+&copy; Designed by [Hao-Ting Tso](https://davidtso.com)
+
