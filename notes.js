9275f81155b66af414280423354e1cfc8f4a6bae
david tso <davidtso1219@gmail.com>
2022-01-09 03:53:45 +0800

    Switched back to my design

--- notes.js
+++ notes.js
@@ -14,14 +14,6 @@ function decodeHtml(html) {
 
 async function main() {
 
-    const ps = document.getElementsByTagName('p')
-    for (var i = 0; i < ps.length; i++) {
-        var p = ps[i]
-        if (typeof p.previousElementSibling !== 'undefined' && p.previousElementSibling && (p.previousElementSibling.tagName.toLowerCase() === 'p' || p.previousElementSibling.tagName.toLowerCase() == 'ol' || p.previousElementSibling.tagName.toLowerCase() == 'ul')){
-            p.style.margin = 0;
-        }
-    }
-
     window.onload = function() {
         var header = document.getElementsByTagName('h1')[0]
         var content = document.querySelector('.content')
